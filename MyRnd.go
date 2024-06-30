daae68f4cf446651bf6fa03b8203164af4b11eab
matteomarelli91 <matteomarelli91@gmail.com>
2024-06-30 12:21:59 +0200

    no message

--- MyRnd.go
+++ MyRnd.go
@@ -8,7 +8,6 @@ import (
 func GetNumbRnd() int {
 	app1 := time.Now()
 	app := app1.Nanosecond()
-	//fmt.Printf("%d %T", app, app)
 	s3 := rand.NewPCG(0, (uint64)(app))
 	r3 := rand.New(s3)
 	return r3.IntN(100)
